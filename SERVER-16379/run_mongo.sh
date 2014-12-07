16e7706a4aafc0d9eeb119295dd7c7035ae606af
Alex Gorrod <alexg@wiredtiger.com>
2014-12-07 23:54:22 +0000

    Make it easier to switch engines and sleep before starting mongos.

--- SERVER-16379/run_mongo.sh
+++ SERVER-16379/run_mongo.sh
@@ -6,9 +6,12 @@ mkdir -p db/s0r0
 mkdir -p db/s1r0
 mkdir -p db/c
 
-$MONGODB_BIN/mongod --dbpath ./db/s0r0 --logpath ./db/s0r0.log --logappend --port 30000  --storageEngine wiredtiger --fork
-$MONGODB_BIN/mongod --dbpath ./db/s1r0 --logpath ./db/s1r0.log --logappend --port 31000  --storageEngine wiredtiger --fork
+ENGINE=wiredtiger
+
+$MONGODB_BIN/mongod --dbpath ./db/s0r0 --logpath ./db/s0r0.log --logappend --port 30000  --storageEngine $ENGINE --fork
+$MONGODB_BIN/mongod --dbpath ./db/s1r0 --logpath ./db/s1r0.log --logappend --port 31000  --storageEngine $ENGINE --fork
 $MONGODB_BIN/mongod --configsvr --dbpath db/c --logpath db/c.log --logappend --fork
+sleep 1
 $MONGODB_BIN/mongos --port 27017 --configdb localhost --logpath db/ms0.log --logappend  --noAutoSplit --fork
 
 
