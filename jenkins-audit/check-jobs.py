7b3e5a7c999c123069574e9a83551a16ae884427
David Hows <howsdav@gmail.com>
2016-08-18 11:02:41 +1000

    Add check for enable-strict on jobs

--- jenkins-audit/check-jobs.py
+++ jenkins-audit/check-jobs.py
@@ -1,6 +1,15 @@
 #! /usr/bin/python
 import xml.etree.ElementTree
 import os
+
+strcit_skips = [
+    "wiredtiger-osx",
+    "wiredtiger-compat-r1",
+    "wiredtiger-compat-r2",
+    "wiredtiger-compat-develop",
+    "wiredtiger-test-mongodb-3.0",
+    "wiredtiger-test-mongodb-3.2"
+]
 job_dir = "/home/jenkins/jenkins/jobs/"
 def check_recips (recips):
     if recips == None:
@@ -19,7 +28,7 @@ def check_mail (root):
     if mailer == None:
         mailer = root.find('./publishers/hudson.plugins.emailext.ExtendedEmailPublisher')
         if mailer == None:
-            print("Error, job %s has no email notification setup" % job)
+            print("Error; Job %s has no email notification setup" % job)
             return True
         recips = mailer.find("recipientList")
     else:
@@ -27,11 +36,51 @@ def check_mail (root):
     if check_recips(recips):
         return True
 
+def check_configure(text):
+    if "configure" in text:
+        return True
+
+def check_strict(text):
+    # Skip those tests which set explicit warnings
+    if "$warnings" in text:
+        return True
+    if "-W" in text:
+        return True
+    if "CC=\"clang" in text:
+        return True
+    if "enable-strict" in text:
+        return True
+    return False
+
+
+def check_builds(root):
+    builders = root.find('./builders')
+    # Check if this test builds something
+    if builders == None:
+        return True
+    found_compile = False
+    found_strict = False
+    for b in list(builders):
+        command = b.find("./command")
+        if command == None:
+            continue
+        if check_configure(command.text):
+            found_compile = True
+            if check_strict(command.text):
+                found_strict = True
+    # If the job doesn't do a WT compile, there is no issue
+    if not found_compile:
+        return True
+    return found_strict
+
+
 for job in os.listdir(job_dir):
     config_file = job_dir + job + "/config.xml"
-    if not os.path.exists(config_file): 
+    if not os.path.exists(config_file):
         print("Old Job found - %s" % job)
         continue
     root = xml.etree.ElementTree.parse(config_file).getroot()
-    if check_mail(root):
-        continue
+    check_mail(root)
+    if job not in strcit_skips:
+        if not check_builds(root):
+            print("Error; Builder(s) in job %s don't have --enable-strict" % job)
