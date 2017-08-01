41b3d82641d3b091e7ef2a85cdaa0c26f235862f
David Hows <howsdav@gmail.com>
2017-08-01 16:55:23 +1000

    Add a stats test and a test for mongodb perf branches

--- jenkins-audit/check-jobs.py
+++ jenkins-audit/check-jobs.py
@@ -1,7 +1,10 @@
 #! /usr/bin/python
+
 import xml.etree.ElementTree as etree
 import os
 import re
+import requests
+import json
 
 strcit_skips = [
     "wiredtiger-osx",
@@ -33,6 +36,8 @@ clean_skips = [
 ]
 
 match_jobs_set = False
+
+github_branches = []
 
 job_dir = "/home/jenkins/jenkins/jobs/"
 def check_recips (recips):
@@ -116,6 +121,30 @@ def check_matches(root, job):
             return False
     return True
 
+def check_archiving_perf_jobs(root, job):
+    found_stats = False
+    plots = root.find('./publishers/hudson.plugins.plot.PlotPublisher') 
+
+    # If there is no plotting, we don't care
+    if plots == None:
+        return True
+
+    archives = root.find('./publishers/hudson.tasks.ArtifactArchiver/artifacts')
+
+    # If there is nothing archived, then we have failed
+    if archives == None:
+        return False
+
+    if "diagdata" in archives.text:
+        found_stats = True
+
+    if "diagnostic.data" in archives.text:
+        found_stats = True
+
+    if "WiredTigerStat" in archives.text:
+        found_stats = True
+
+    return found_stats
 
 def check_builds(root, job):
 
@@ -151,13 +180,37 @@ def check_builds(root, job):
     
     return found_strict
 
+# Remove MongoDB branches that are begin tested
+def check_mongodb_builds(root, job):
+    global github_branches
+    if "mongodb-perf-ycsb-mongodb-" not in job:
+        return
+    m = re.search('(\d\.\d)', job)
+    branch = m.groups(0)[0]
+    if branch not in github_branches:
+        print("Error; Job %s doesn't have a matching branch on Github" % job)
+    github_branches.remove(branch)
 
+# Grab all the mognodb branches on github into a list to remove from later
+def setup_mongodb_branches():
+    global github_branches
+    r = requests.get('https://api.github.com/repos/wiredtiger/wiredtiger/branches')
+    if(r.ok):
+        repoItem = json.loads(r.text or r.content)
+        for i in repoItem:
+            if "mongodb-" in i["name"]:
+                m = re.search('(\d\.\d)', i["name"])
+                github_branches.append(m.groups(0)[0])
+
+# Main
+setup_mongodb_branches()
 for job in os.listdir(job_dir):
     config_file = job_dir + job + "/config.xml"
     if not os.path.exists(config_file):
         print("Old Job found - %s" % job)
         continue
     root = etree.parse(config_file).getroot()
+    check_mongodb_builds(root, job)
     for match in match_jobs:
         if job == match[0]:
             check_matches(root, job)
@@ -167,3 +220,9 @@ for job in os.listdir(job_dir):
         print("Error; Builder(s) in job %s don't have --enable-strict" % job)
     if not check_email_plots(root, job):
         print("Error; Email of plots in job %s doesn't have the correct URL" % job)
+    if not check_archiving_perf_jobs(root, job):
+        print("Error; perf job %s doesn't archive stats" % job)
+
+# We should have at max 1 branch, the current head of mongodb, which is tested by develop
+if len(github_branches) > 1:
+    print("Error; There are branches that aren't tested by mongodb-perf Jenkins jobs")
