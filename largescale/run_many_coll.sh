7d36ca5d50b89306c4fbe1ba266e9ae071237b87
Etienne Petrel <etienne.petrel@mongodb.com>
2021-07-01 00:46:51 +0000

    Keep output folder if already existing. Added seconds to the output filename.

--- largescale/run_many_coll.sh
+++ largescale/run_many_coll.sh
@@ -64,7 +64,7 @@ mkdir results
 # Create folder if needed
 mkdir -p "$OUTPUT"
 cd "$OUTPUT" || exit
-mkdir -p mkdir dbpath;
+mkdir -p dbpath;
 
 # Try to reuse existing mongod process
 if pgrep -x "mongod" > /dev/null; then
@@ -79,11 +79,11 @@ python3 ../many-collection-test.py ../"$TEST_CFG"
 
 # Save generated files
 cd ..
-BAK_DIR="$OUTPUT"-"$(date +%F-%H:%M)"
-mkdir "$BAK_DIR"
+BAK_DIR="$OUTPUT"-"$(date +%F-%H:%M:%S)"
+mkdir -p "$BAK_DIR"
 mv results/ "$BAK_DIR"/.
 cp -r "$OUTPUT"/dbpath/diagnostic.data "$BAK_DIR"/.
-mkdir "$BAK_DIR"/cfg
+mkdir -p "$BAK_DIR"/cfg
 cp "$TEST_CFG" "$BAK_DIR"/cfg/.
 
 # Clean temporary files
